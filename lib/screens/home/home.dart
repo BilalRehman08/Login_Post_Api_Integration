9330183ba022d178673fd74246dda040ade434f7
BilalRehman08 <bilalrehman080808@gmail.com>
2021-05-21 04:58:01 -0700

    post api integration

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task/models/user.dart';
+import 'package:task/screens/home/productdetails.dart';
 import 'package:task/services/remote/api.dart';
 
 class Home extends StatelessWidget {
@@ -28,7 +29,7 @@ class __BodyState extends State<_Body> {
   submitForm() async {
     ApiService _apiService = ApiService();
 
-    List<dynamic> eachposts = await _apiService.getUserPosts(10);
+    List<dynamic> eachposts = await _apiService.getUserPosts(5);
     setState(() {
       allposts = eachposts;
     });
@@ -45,15 +46,83 @@ class __BodyState extends State<_Body> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("${allposts[0]['id']}"),
-      ),
       body: ListView.builder(
           itemCount: allposts.length,
           itemBuilder: (context, i) {
-            return ListTile(
-              title: Text("${allposts[i]['title']}"),
-            );
+            return Card(
+                child: Hero(
+                    tag: i,
+                    child: GestureDetector(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ProductDetails(
+                                  "${allposts[i]['id']}",
+                                  allposts[i]['title']))),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            height: 130,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                  image: NetworkImage(allposts[i]['title']),
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Container(
+                            height: 25,
+                            child: Text(allposts[i]['title'],
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 23)),
+                          ),
+                          SizedBox(height: 7),
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.yellowAccent,
+                                  size: 11,
+                                ),
+                                SizedBox(width: 1),
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.yellowAccent,
+                                  size: 11,
+                                ),
+                                SizedBox(width: 1),
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.yellowAccent,
+                                  size: 11,
+                                ),
+                                SizedBox(width: 1),
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.yellowAccent,
+                                  size: 11,
+                                ),
+                                SizedBox(width: 1),
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.yellowAccent,
+                                  size: 11,
+                                ),
+                                SizedBox(width: 2),
+                                Text(
+                                  "5.0 (23 Reviews)",
+                                  style: TextStyle(fontSize: 11),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )));
           }),
     );
   }
